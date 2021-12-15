ba335c5b2e0439c39b59fa5640a7d3239f52b737
mkarenina <mariannakarenina@usp.br>
2021-12-15 09:42:35 -0300

    funcionalidade 8 aiaiai

--- main.c
+++ main.c
@@ -684,7 +684,8 @@ void Dijkstra(char origem[], char destino[], vertice *vet[], int total_vertices,
 	while(pos!=-1 && strcmp(ANT[pos], "\0")!=0){
         cont ++;
 		pos=percorreVertice(ANT[pos], vet);
-		if(strcmp(ANT[pos], destino)==0 || strcmp(ANT[pos], origem)==0){
+		//if(strcmp(ANT[pos], destino)==0 || strcmp(ANT[pos], origem)==0){
+			if( strcmp(ANT[pos], origem)==0){
 			break;
 		}
 	}
